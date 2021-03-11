5cead097d72debe9a783330e7af730c9a5f7ae8d
Shelly Grossman <shellygr151@gmail.com>
2021-03-11 12:13:06 +0200

    apply harness

--- spec/scripts/applyHarnesses.sh
+++ spec/scripts/applyHarnesses.sh
@@ -1,3 +1,8 @@
 # SafeTransfer simplification
-sed -i 's/safeT/t/g' contracts/BentoBoxPlus.sol
-sed -i 's/safeT/t/g' contracts/LendingPair.sol
+#sed -i 's/safeT/t/g' contracts/BentoBoxPlus.sol
+#sed -i 's/safeT/t/g' contracts/LendingPair.sol
+# Virtualize functions
+perl -0777 -i -pe 's/public payable \{/public virtual payable \{/g' node_modules/@sushiswap/bentobox-sdk/contracts/BentoBoxV1.sol
+perl -0777 -i -pe 's/external payable returns/external virtual payable returns/g' node_modules/@sushiswap/bentobox-sdk/contracts/BentoBoxV1.sol
+perl -0777 -i -pe 's/external view returns \(uint256 /external virtual view returns \(uint256 /g' node_modules/@sushiswap/bentobox-sdk/contracts/BentoBoxV1.sol
+perl -0777 -i -pe 's/uint256\[\] calldata amounts,\s+bytes calldata data\s+\) public/uint256\[\] calldata amounts,bytes calldata data\) public virtual/g' node_modules/@sushiswap/bentobox-sdk/contracts/BentoBoxV1.sol 
